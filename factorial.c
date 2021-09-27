55a6c9e12d14af10c3cdf29436d2449a4952a579
er-ssmohanty <pie2001@protonmail.com>
2021-09-27 21:18:04 +0530

    updated factorial finder

--- factorial.c
+++ factorial.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main(){
-    int num,factorial0=1;
+    int num,factorial=1;
     printf("Enter a number: ");
     scanf("%d",&num);
     while (num!=1)
     {
-        factorial0=factorial0*num;
+        factorial=factorial*num;
         num=num-1;
         /* code */
     }
-    printf("%d\n",factorial0);
+    printf("%d\n",factorial);
     //return factorial0;s
 }
