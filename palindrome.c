704a8c6731ca7ec88f8aa1429ce730097bb6136f
er-ssmohanty <pie2001@protonmail.com>
2021-09-28 09:30:07 +0530

    vitalised palindrome

--- palindrome.c
+++ palindrome.c
@@ -1,20 +1,53 @@
 #include<stdio.h>
+#include<math.h>
+int digitcount(int num){
+    int a=log10(num)+1;
+    return a;
+}
+void oddhandle(int num, int digcount){
+    int digits[digcount];
+    for (int i = digcount-1; i >= 0; i--)
+    {
+        digits[i]=num%10;
+        num=num/10;
+    }
+    
+    
+}
+void evenhandle(int num, int digcount){
+    int digits[digcount];
+    int left[digcount/2];
+    int right[digcount/2];
+    for (int i = digcount-1; i >= 0; i--)
+    {
+        digits[i]=num%10;
+        num=num/10;
+    }
+    for (int i = 0; i < digcount/2; i++)
+    {
+        right[i]=digits[i];
+    }
+    for (int i = digcount/2; i < digcount; i++)
+    {
+        left[i]=digits[i+(digcount/2)];
+    }
+    
+    
 
+}
 int main()
 {
-    int num, rev;
+    int num,digcount; //rev;
     printf("Enter a number: ");
     scanf("%d",&num);
-    //1234321 
-    int place=0;
-    while (num/place>=1)
+    digcount = digitcount(num);
+    if (digcount%2==0)
     {
-        num=num/10;
-        
+        evenhandle(num,digcount);
     }
-    printf("%d\n",place);
-    
-    
-
-    
+    else
+    {
+        oddhandle(num,digcount);
+    }
+    return 0;
 }
