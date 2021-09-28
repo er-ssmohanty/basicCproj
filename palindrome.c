860f9db868383339a9566f8bf9a1e30a01a3bd86
er-ssmohanty <pie2001@protonmail.com>
2021-09-28 21:15:58 +0530

    updated palindrome

--- palindrome.c
+++ palindrome.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<math.h>
+#include<string.h>
 int digitcount(int num){
     int a=log10(num)+1;
     return a;
@@ -18,21 +19,50 @@ void evenhandle(int num, int digcount){
     int digits[digcount];
     int left[digcount/2];
     int right[digcount/2];
-    for (int i = digcount-1; i >= 0; i--)
-    {
+    int res=1,test=0;
+    
+    for (int i = 0; i < digcount; i++)
+    {//digit string
         digits[i]=num%10;
         num=num/10;
     }
+
+
     for (int i = 0; i < digcount/2; i++)
     {
         right[i]=digits[i];
     }
-    for (int i = digcount/2; i < digcount; i++)
+        
+    for (int i = 0; i < digcount/2; i++)
     {
-        left[i]=digits[i+(digcount/2)];
+        left[i]=digits[i+digcount/2];
     }
     
+    for (int i = 0; i < digcount; i++)
+    {
+            if (left[i]==right[digcount-i])
+            {
+                res=res*1;
+                test=test+1;
+            }
+            else
+            {
+                res=res*0;
+            }
+            
+            
+    }
     
+    if (res==1)
+    {
+        printf("\nIt is a palindrome number.\n");
+    }
+    else
+    {
+        printf("\nIt not is a palindrome number.\n");
+    }
+    
+    printf("%d\n",test);
 
 }
 int main()
