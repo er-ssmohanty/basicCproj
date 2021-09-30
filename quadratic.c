f687848ee02e2440734f67ad001572b84457fe69
er-ssmohanty <pie2001@protonmail.com>
2021-09-30 19:09:06 +0530

    added quadratic equation solver

---
+++ quadratic.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    double a,b,c,del,alpha,beta;
+    printf("Enter 'a' or the coefficient of x^2: ");
+    scanf("%lf",&a);
+    printf("Enter 'b' or the coefficient of x: ");
+    scanf("%lf",&b);
+    printf("Enter 'c' or the constant value: ");
+    scanf("%lf",&c);
+    if ((b*b)-(4*a*c) < 0)
+    {
+        printf("No real solutions!\n");
+        return 1;
+    }
+    else
+    {
+        printf("Alpha = %lf and Beta = %lf\n",(-b+sqrt((b*b)-(4*a*c)))/(2*a),(-b-sqrt((b*b)-(4*a*c)))/(2*a));
+    }
+}
